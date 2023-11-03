7c644a406f73e68129ff6a04660fee3c498a5560
hntk03 <hntk03@gmail.com>
2023-11-03 16:29:24 +0900

    first

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,78 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { Container, RadioGroup, Stack, Radio, Input, Button, Textarea } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function Home() {
+	const [value, setValue] = useState('1')
+	const [condition1, setCondition1] = useState('')
+	const [condition2, setCondition2] = useState('')
+	const [text, setText] = useState()
+	const [conditionNum, setConditionNum] = useState(2)
+
+const onChangeCondition1 = (e) => {
+	setCondition1(e.target.value)
+	generate(value)
+}
+
+const onChangeCondition2 = (e) => {
+	setCondition2(e.target.value)
+	generate(value)
+}
+
+const onChangeType = (e) => {
+	console.log(e)
+	setValue(e)
+	generate(e)
+}
+
+const generate = (value) =>{
+	let type = ""
+	if(value == 1){
+		type = "AND"
+	} 
+	else{
+		type = "OR";
+	}
+
+	const first = type + " ┬ " + condition1
+	const end = makeSpace(type) + " └ " + condition2
+	const content = [first, end].join("\n")
+	setText(content)
+}
+
+const makeSpace = (type) => {
+	return Array(type.length+4).fill(" ").join("")
+}
+
   return (
-    <div className={styles.container}>
+		<ChakraProvider>
+    <div>
+			<Container>
       <Head>
-        <title>Create Next App</title>
+        <title>AND OR Format</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+			<RadioGroup onChange={onChangeType} value={value}>
+				<Stack direction='row'>
+					<Radio value='1'>AND</Radio>
+					<Radio value='2'>OR</Radio>
+				</Stack>
+			</RadioGroup>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+			<Stack spacing={3}>
+				<Input placeholder='条件1' size='md' onChange={onChangeCondition1} value={condition1} />
+				<Input placeholder='条件2' size='md' onChange={onChangeCondition2} value={condition2} />
+			</Stack>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+			<Textarea placeholder='' value={text} isReadOnly={true} size="lg" />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+			</Container>
+		</div>
+		</ChakraProvider>
   );
 }
