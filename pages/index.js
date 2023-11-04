8c6d45520f4390e3089c53c3f4975fb67736ad78
hntk03 <hntk03@gmail.com>
2023-11-04 09:55:58 +0900

    modify removeButton

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,15 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { Container, RadioGroup, Stack, Radio, Input, Button, Textarea } from '@chakra-ui/react';
+import { Container, RadioGroup, Stack, Radio, Input, Button, CloseButton, Textarea } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useState } from 'react';
 
 export default function Home() {
-	const conditionNumMax = 5
-	const conditionNumMin = 2
 	const [value, setValue] = useState('1')
-	const [condition1, setCondition1] = useState('')
-	const [condition2, setCondition2] = useState('')
-	const [conditions, setConditions] = useState(Array(conditionNumMax).fill(''))
+	const conditionNumMin = 2
+	const conditionNumMax = 5
+	const [conditions, setConditions] = useState(Array(conditionNumMin).fill(''))
 	const [text, setText] = useState()
-	const [conditionNum, setConditionNum] = useState(conditionNumMin)
-
-const onChangeCondition1 = (e) => {
-	console.log(e)
-	setCondition1(e.target.value)
-	generate(value)
-}
 
 const onChangeCondition = (e) => {
 	const data = conditions.slice()
@@ -31,24 +22,32 @@ const onChangeCondition = (e) => {
 	generate(value, data)
 }
 
-const onChangeCondition2 = (e) => {
-	setCondition2(e.target.value)
-	generate(value)
-}
-
 const onChangeType = (e) => {
 	setValue(e)
 	generate(e, conditions)
 }
 
 const onClickAdd = () => {
-	const num = Math.min(conditionNum+1, conditionNumMax)
-	setConditionNum(num)
+	if(conditions.length+1 > conditionNumMax){
+		return 
+	}
+
+	conditions.push('')
+
+	generate(value, conditions)
 }
 
-const onClickDelete = () => {
-	const num = Math.max(conditionNum-1, conditionNumMin)
-	setConditionNum(num)
+const onClickCloseButton = (e) => {
+	if(conditions.length-1 < conditionNumMin){
+		return
+	}
+
+	const delIndex = Number(e.target.id) - 1
+	const copyConditions = conditions.slice()
+	copyConditions.splice(delIndex, 1)
+
+	setConditions(copyConditions)
+	generate(value, copyConditions)
 }
 
 const generate = (value, conditions) =>{
@@ -63,41 +62,38 @@ const generate = (value, conditions) =>{
 	const spaces = makeSpace(type)
 
 	const list = []
-	const first = type + " ┬ " + conditions[0]
-	const end = spaces + " └ " + conditions[conditionNum-1]
+	const first = `${type} ┬ ${conditions[0]}`
+	const end = `${spaces} └  ${conditions[conditions.length-1]}`
 
-	for(let i=1;i<conditionNum-1;i++){
-		const item = spaces + "  ├ " + conditions[i]
+	for(let i=1;i<conditions.length-1;i++){
+		const item = `${spaces}  ├ ${conditions[i]}`
 		list.push(item)
 	}
 
 	list.unshift(first)
   list.push(end)
 
-
 	setText(list.join('\n'))
-	// const first = type + " ┬ " + condition1
-	// const end = makeSpace(type) + " └ " + condition2
-	// const content = [first, end].join("\n")
-	// setText(content)
 }
 
 const makeSpace = (type) => {
 	return Array(type.length+4).fill(" ").join("")
 }
 
-const hoge = () => {
+const DisplayConditions = () => {
 	const list = []
-	for(let i=0; i<conditionNum; i++){
+	for(let i=0; i<conditions.length; i++){
 		const placeholder = '条件' + (i+1)
-		list.push(<Input key={i+1} id={i+1} placeholder={placeholder} size='md' onChange={onChangeCondition} value={conditions[i]} />)
-	}
-	return list
-}
+		const row = []
+		row.push(<Stack key={`stack_${i+1}`} direction='row'>)
+		row.push(<Input key={`input_${i+1}`} id={i+1} placeholder={placeholder} size='md' onChange={onChangeCondition} value={conditions[i]} />)
+		row.push(<CloseButton key={`closeButton_${i+1}`} id={i+1} onClick={onClickCloseButton} />)
+		row.push(</Stack>)
+		list.push(row)
 
-const tmp = () => {
-	// <Input placeholder='条件1' size='md' onChange={onChangeCondition1} value={condition1} />
-	// <Input placeholder='条件2' size='md' onChange={onChangeCondition2} value={condition2} />
+	}
+
+	return list
 }
 
   return (
@@ -118,12 +114,11 @@ const tmp = () => {
 			</RadioGroup>
 
 			<Stack spacing={3}>
-				{hoge()}
+				{DisplayConditions()}
 			</Stack>
 
 			<Stack direction='row'>
 				<Button onClick={onClickAdd}>追加</Button>
-				<Button onClick={onClickDelete}>削除</Button>
 			</Stack>
 
 			<Textarea h='calc(30vh)' placeholder='' value={text} isReadOnly={true} size="lg" />
